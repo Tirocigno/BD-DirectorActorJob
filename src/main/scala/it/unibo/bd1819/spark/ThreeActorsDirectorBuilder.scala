26d367f43afc690dfa9dfac9acffbc5ffa70aae0
Tirocigno <fede.naldini1@gmail.com>
2019-06-06 10:50:45 +0200

    Refactored with a better name

--- src/main/scala/it/unibo/bd1819/spark/ThreeActorsDirectorBuilder.scala
+++ src/main/scala/it/unibo/bd1819/spark/ThreeActorsDirectorBuilder.scala
@@ -24,9 +24,6 @@ object ThreeActorsDirectorBuilder {
 
   private class ThreeActorsDirectorBuilderImpl(sqlContext: SQLContext) extends ThreeActorsDirectorBuilder {
 
-    var directorActorCounterMap: scala.collection.mutable.Map[String, DirectorEntryValue] =
-      scala.collection.mutable.Map()
-
     override def buildThreeActorsDirectorsDataFrame(initialDataFrame: DataFrame): DataFrame = {
 
       val threePartitionRDD = initialDataFrame.rdd.map(row => {
@@ -36,55 +33,14 @@ object ThreeActorsDirectorBuilder {
         (directorID, (actorID, collabCount))
       }).groupByKey
         .map {
-          case (key, number) => key -> number.toList.sortBy(-_._2).take(3)
+          case (directorID, actorsCollabIterable) => directorID -> actorsCollabIterable.toList.sortBy(-_._2).take(3)
         }
         .flatMap {
-          case (key, numbers) => numbers.map(key -> _)
+          case (directorID, topThreeActorList) => topThreeActorList.map(directorID -> _)
         }.map(keyvaluerow =>
         Row(keyvaluerow._1, keyvaluerow._2._1, keyvaluerow._2._2))
       sqlContext.createDataFrame(threePartitionRDD, initialDataFrame.schema)
     }
-
-    case class DirectorEntryValue(var topThree: (ActorCollabRecord, ActorCollabRecord, ActorCollabRecord) =
-                                  (ActorCollabRecord(), ActorCollabRecord(), ActorCollabRecord())) {
-
-      /**
-        * Process a new record updating the topThree actors-collab records.
-        *
-        * @param record the record to process.
-        */
-      def processNewRecord(record: ActorCollabRecord): Unit = {
-        if (record.actorCollab > topThree._1.actorCollab) {
-          topThree = (record, topThree._1, topThree._2)
-        } else {
-          if (record.actorCollab > topThree._2.actorCollab) {
-            topThree = (topThree._1, record, topThree._2)
-          } else {
-            if (record.actorCollab > topThree._3.actorCollab) {
-              topThree = (topThree._1, topThree._2, record)
-            }
-          }
-        }
-      }
-
-      /**
-        * Check if the topthree records contains a specified record
-        *
-        * @param actorCollabRecord the recors to check
-        * @return true if the record is present, false otherwise
-        */
-      def contains(actorCollabRecord: ActorCollabRecord): Boolean = {
-        actorCollabRecord.equals(topThree._1) ||
-          actorCollabRecord.equals(topThree._2) ||
-          actorCollabRecord.equals(topThree._3)
-      }
-
-
-    }
-
-    case class ActorCollabRecord(actorID: String = "", actorCollab: Long = 0)
-
-    case class DirectorActorCountTuple(directorID: String, actorID: String, actorCollab: Long)
 
   }
 
