c536da876cb400921515dd732d4f7536dd566df8
Tirocigno <fede.naldini1@gmail.com>
2019-06-10 11:17:52 +0200

    Scallop should have been configurated

--- src/main/scala/it/unibo/bd1819/ScalaMain.scala
+++ src/main/scala/it/unibo/bd1819/ScalaMain.scala
@@ -4,12 +4,34 @@ import it.unibo.bd1819.spark.ThreeActorsDirectorBuilder
 import utils.DFFactory._
 import org.apache.spark.{SparkContext, sql}
 import org.apache.spark.sql.{Row, SQLContext, SparkSession}
+import org.rogach.scallop.ScallopConf
+
+/**
+  * Class to be used to parse CLI commands, the values declared inside specify name and type of the arguments to parse.
+  *
+  * @param arguments the programs arguments as an array of strings.
+  */
+class Conf(arguments: Seq[String]) extends ScallopConf(arguments) {
+  val executors = opt[Int]()
+  val tasks = opt[Int]()
+  verify()
+}
 
 
 object ScalaMain extends App {
 
   var executors = 2
   var taskForExceutor = 4
+  val conf = new Conf(args)
+
+  if(conf.executors.supplied) {
+    executors = conf.executors()
+  }
+
+  if(conf.tasks.supplied) {
+    taskForExceutor = conf.tasks()
+  }
+
   val sc =  new SparkContext()
   val sqlContext = SparkSession.builder.getOrCreate.sqlContext
   val titleBasicsDF = getTitleBasicsDF(sc, sqlContext)
